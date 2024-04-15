0747d36b03ed9dbdaf829a44a85c30b910c1647b
길재혁 <81075170+wogur2689@users.noreply.github.com>
2024-04-15 16:57:18 +0900

    [fix] 컨트롤러 검증, 체크 추가

--- src/main/java/com/example/bookbackend/reading/application/dto/ReadingRequestDto.java
+++ src/main/java/com/example/bookbackend/reading/application/dto/ReadingRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.bookbackend.reading.application.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,5 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReadingRequestDto {
     //이건 유저정보에서 조회하는거라 솔직히 필요없음.
+    @NotBlank(message = "닉네임을 입력해주세요.")
     private String nickName;
 }
--- src/main/java/com/example/bookbackend/reading/web/ReadingController.java
+++ src/main/java/com/example/bookbackend/reading/web/ReadingController.java
@@ -8,6 +8,8 @@ import com.example.bookbackend.reading.application.dto.ReadingResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,7 +32,22 @@ public class ReadingController {
     //메인페이지 읽은 책 데이터 조회
     @GetMapping("/getReadingData")
     public CommonResponse getReadingData(@Valid @RequestBody ReadingRequestDto readingRequestDto, BindingResult result) {
+        //ApiCode로 빠질 예정.
+        //1. 검증오류 체크
+        if(result.hasErrors()) {
+            log.error("result 검증 오류 : {} ", result.getFieldError().getDefaultMessage());
+            return new CommonResponse(ApiCode.API_9999.getCode(), result.getFieldError().getDefaultMessage());
+        }
+        //2. 로그인 세션 체크
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth == null) {
+            return new CommonResponse(ApiCode.API_9999.getCode(), "로그인 해주세요.");
+        }
+
+        //3. 데이터 조회
         ReadingResponseDto readingResponseDto = readingService.returnMainData(readingRequestDto);
+
+        //return
         return new CommonResponse(ApiCode.API_0000.getCode(), ApiCode.API_0000.getMsg());
     }
 }
