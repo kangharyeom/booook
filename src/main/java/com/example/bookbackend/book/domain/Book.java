bd3a61f9d6434b1e62698ed5d8011a2f588db6bd
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-04-18 23:41:49 +0900

    [feat] BookEntity 생성

--- src/main/java/com/example/bookbackend/book/domain/Book.java
+++ src/main/java/com/example/bookbackend/book/domain/Book.java
@@ -1,4 +1,34 @@
 package com.example.bookbackend.book.domain;
 
-public class Book {
+import com.example.bookbackend.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "BOOK")
+public class Book extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bookId;
+    private String title;
+    private String startDate;
+    private String endDate;
+    private String summary;
+    private String text;
+    private String imageUrl;
+    private int totalPageCount;
+    private int goalPageCount;
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.REMOVE)
+    private List<BookMarker> bookMarkerList = new ArrayList<>();
+
+//    @ManyToOne(cascade = CascadeType.DETACH)
+//    @JoinColumn(name = "MEMBER_ID")
+//    private Member member;
 }
