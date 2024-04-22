e879d21655ff11bd58259d16f5b6450cdc8a55a9
jonghunbaek <qor6028@naver.com>
2024-04-22 22:31:01 +0900

    feat : token으로 조회하는 메서드 추가

--- src/main/java/com/example/bookbackend/token/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/bookbackend/token/repository/RefreshTokenRepository.java
@@ -3,6 +3,9 @@ package com.example.bookbackend.token.repository;
 import com.example.bookbackend.token.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
+    Optional<RefreshToken> findByToken(String refreshToken);
 }
