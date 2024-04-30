83d6da8d78f38d173174a182f8c448c7a2860f93
길재혁 <81075170+wogur2689@users.noreply.github.com>
2024-04-30 17:01:25 +0900

    test : 테스트 케이스 생성

---
+++ src/test/java/com/example/bookbackend/reading/repository/ReadingRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.example.bookbackend.reading.repository;
+
+import com.example.bookbackend.common.exception.GlobalException;
+import com.example.bookbackend.common.util.ApiCode;
+import com.example.bookbackend.reading.domain.Reading;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@DataJpaTest
+class ReadingRepositoryTest {
+
+    @Autowired
+    private ReadingRepository readingRepository;
+
+    @DisplayName("특정 유저가 얼만큼 읽었는지 가져온다.")
+    @Test
+    void findByNickname() {
+        //given
+        Reading reading = Reading.builder()
+                .bookTitle("편의점 가는 기분")
+                .nickName("test")
+                .pageNo(20)
+                .build();
+
+        //when
+        try {
+            readingRepository.save(reading);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("DB error : " + e.getMessage());
+        }
+
+        List<Reading> readings = readingRepository.findByNickname("test").
+                orElseThrow(() -> new GlobalException(ApiCode.API_9999));
+
+        //then
+        for(Reading r : readings) {
+            assertThat(r.getNickName()).isEqualTo("test");
+        }
+    }
+
+    @Test
+    void findByBookTitle() {
+    }
+}
